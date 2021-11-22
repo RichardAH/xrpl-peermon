a86cfff5589bfab3d48d0324107babb0aaedbf8b
Richard Holland <richard.holland@starstone.co.nz>
2021-11-22 11:23:41 +0000

    first version

--- stress.sh
+++ stress.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
+which wscat > /dev/null 2>/dev/null
+if [ "$?" -ne "0" ]
+then
+    echo 'wscat is required to use this script'
+    exit 1
+fi
+
+which ./peermon > /dev/null 2> /dev/null
+if [ "$?" -ne "0" ]
+then
+    echo 'Please compile peermon first then run this script from the build directory.'
+    exit 1
+fi
+
+
+if [ "$#" -ne "3" ]
+then
+    echo "rippled stress tester"
+    echo "Usage: $0 peer-ip peer-port ws-endpoint"
+    echo "Example: $0 r.ripple.com 51235 ws://localhost:6006"
+    exit 1
+fi
+
+PIPE=$(mktemp -u)
+
+
+echo "Starting stress tester..."
+
+mkfifo $PIPE
+if [ "$?" -ne "0" ];
+then
+    echo "Failed to create fifo at $PIPE"
+    exit 2
+fi
+echo "Created fifo: $PIPE"
+
+exec 3<>$PIPE
+/bin/rm -f $PIPE
+
 AWK_SCRIPT='
 {
     if ($1 == "Account" || $1 == "Destination")
@@ -12,6 +51,8 @@ AWK_SCRIPT='
         print("{\"command\": \"ledger\", \"ledger_hash\":\""$2"\"}")
     }
 }'
-echo "$SCRIPT"
-./peermon r.ripple.com 51235 no-cls no-stats | grep -E --line-buffered 'LedgerHash|Account|Destination' | 
-    tr -d '",:' | awk "$AWK_SCRIPT"
+
+wscat -c $3 <&3 &
+wscat=$?
+./peermon $1 $2 no-cls no-stats | grep -E --line-buffered 'LedgerHash|Account|Destination' | 
+    tr -d '",:' | awk "$AWK_SCRIPT" >&3
